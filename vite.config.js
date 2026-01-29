73ed5350b20ebde31d4973a33079f6ede06d9805
LBA-OFFICIAL <lbaofficial.secretary@gmail.com>
2026-01-29 18:28:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This ensures assets load correctly on GitHub Pages or subfolders
+  base: './', 
 })
