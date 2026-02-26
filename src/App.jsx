b7770a8503dc4fe95fea366b2b273288d756fd32
LBA-OFFICIAL <lbaofficial.secretary@gmail.com>
2026-02-26 15:24:54 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from './firebase';
 import Login from './components/Login.jsx';
-import Dashboard from './Dashboard';
+import Dashboard from './Dashboard.jsx';
 
 export default function App() {
   const [user, setUser] = useState(null);
